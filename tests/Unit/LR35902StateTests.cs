800366398d087ed23c52216fd96c8db4aa940ca2
c3hmke <cehmke91@gmail.com>
2026-04-22 13:59:43 +0200

    Add some state tests

---
+++ tests/Unit/LR35902StateTests.cs
@@ -0,0 +1,107 @@
+using GameBoy.CPU;
+using Xunit;
+
+namespace Unit;
+
+public sealed class LR35902StateTests
+{
+    [Fact]
+    public void RegisterPairSetters_UpdateComponentRegisters()
+    {
+        var state = new LR35902State
+        {
+            AF = 0x12F7,
+            BC = 0x3456,
+            DE = 0x789A,
+            HL = 0xBCDE
+        };
+
+        Assert.Equal((byte)0x12, state.A);
+        Assert.Equal((byte)0xF0, state.F);
+        Assert.Equal((byte)0x34, state.B);
+        Assert.Equal((byte)0x56, state.C);
+        Assert.Equal((byte)0x78, state.D);
+        Assert.Equal((byte)0x9A, state.E);
+        Assert.Equal((byte)0xBC, state.H);
+        Assert.Equal((byte)0xDE, state.L);
+    }
+
+    [Fact]
+    public void RegisterPairGetters_CombineRegisters()
+    {
+        var state = new LR35902State
+        {
+            AF = 0x1234,
+            BC = 0x5678,
+            DE = 0x9ABC,
+            HL = 0xDEF0
+        };
+
+        Assert.Equal((ushort)0x1230, state.AF);
+        Assert.Equal((ushort)0x5678, state.BC);
+        Assert.Equal((ushort)0x9ABC, state.DE);
+        Assert.Equal((ushort)0xDEF0, state.HL);
+    }
+
+    [Fact]
+    public void SetFlags_SetsExactHighNibble()
+    {
+        var state = new LR35902State();
+
+        state.SetFlags(z: true, n: false, h: true, c: false);
+
+        Assert.Equal((byte)0xA0, state.F);
+        Assert.True(state.FlagZ);
+        Assert.False(state.FlagN);
+        Assert.True(state.FlagH);
+        Assert.False(state.FlagC);
+    }
+
+    [Fact]
+    public void SetFlagOperations_PreserveLowNibbleAsZero()
+    {
+        var state = new LR35902State
+        {
+            AF = 0x00FF
+        };
+
+        state.SetFlagZ(false);
+        state.SetFlagN(true);
+        state.SetFlagH(false);
+        state.SetFlagC(true);
+
+        Assert.Equal((byte)0x50, state.F);
+        Assert.Equal((byte)0x00, (byte)(state.F & 0x0F));
+    }
+
+    [Fact]
+    public void Reset_RestoresDefaultCpuState()
+    {
+        var state = new LR35902State
+        {
+            AF = 0x1230,
+            BC = 0x5678,
+            DE = 0x9ABC,
+            HL = 0xDEF0
+        };
+
+        state.SetFlags(z: false, n: false, h: false, c: false);
+        state.AddClockCycles(1234);
+
+        state.Reset();
+
+        Assert.Equal((ushort)0x0100, state.PC);
+        Assert.Equal((ushort)0xFFFE, state.SP);
+        Assert.Equal((ulong)0, state.CycleCount);
+        Assert.False(state.Halted);
+
+        Assert.Equal((byte)0x00, state.A);
+        Assert.Equal((byte)0x00, state.B);
+        Assert.Equal((byte)0x00, state.C);
+        Assert.Equal((byte)0x00, state.D);
+        Assert.Equal((byte)0x00, state.E);
+        Assert.Equal((byte)0xB0, state.F);
+        Assert.Equal((byte)0x00, state.H);
+        Assert.Equal((byte)0x00, state.L);
+    }
+}
