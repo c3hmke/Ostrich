a922f9aae51be68c0ed69288b39f221b5f6d3d1f
c3hmke <cehmke91@gmail.com>
2026-06-01 14:11:37 +1200

    Add interrupt flag handling to Bus

--- src/GameBoy/Bus.cs
+++ src/GameBoy/Bus.cs
@@ -7,11 +7,11 @@ namespace GameBoy;
 /// </summary>
 public sealed class Bus (Cartridge cartridge)
 {
-    /// <summary> Internal work RAM mapped at 0xC000-0xDFFF. DMG has 8KB in this region. </summary>
-    private readonly byte[] _workRam = new byte[8 * 1024];
+    private readonly byte[] _workRam = new byte[8 * 1024];  // Internal work RAM mapped at 0xC000-0xDFFF. DMG has 8KB in this region.
+    private readonly byte[] _highRam = new byte[127];       // Small scratch region at top of memory mapped at 0xFF80-0xFFFE.
     
-    /// <summary> Small scratch region at top of memory mapped at 0xFF80-0xFFFE. </summary>
-    private readonly byte[] _highRam = new byte[127];
+    private byte _interruptEnable = 0x00;                   // IE at 0xFFFF: which interrupt sources are enabled.
+    private byte _interruptFlags  = 0x00;                   // IF at 0xFF0F: which interrupt sources are currently pending.
 
     /// <summary>
     /// Reads a single byte from the Game Boy address space
@@ -20,14 +20,20 @@ public sealed class Bus (Cartridge cartridge)
     {
         switch (address)
         {
-            case <= 0x7FFF:                             // 0x0000-0x07FF is cartridge controlled ROM space.
+            case <= 0x7FFF:                                 // 0x0000-0x07FF is cartridge controlled ROM space.
                 return cartridge.ReadROM(address);
             
-            case >= 0xC000 and <= 0xDFFF:               // 0xC000-0xDFFF is fixed internal work RAM.
+            case >= 0xC000 and <= 0xDFFF:                   // 0xC000-0xDFFF is fixed internal work RAM.
                 return _workRam[address - 0xC000];
             
-            case >= 0xFF80 and <= 0xFFFE:               // 0xFF80-0xFFFE is high RAM
+            case 0xFF0F:                                    // Interrupt Flag (IF)
+                return _interruptFlags;
+            
+            case >= 0xFF80 and <= 0xFFFE:                   // 0xFF80-0xFFFE is high RAM
                 return _highRam[address - 0xFF80];
+            
+            case 0xFFFF:                                    // Interrupt Enable (IE)
+                return _interruptEnable;
             
             default: throw new NotImplementedException($"Read not implemented for 0x{address:X4}");
         }
@@ -47,8 +53,16 @@ public sealed class Bus (Cartridge cartridge)
                 _workRam[address - 0xC000] = value;
                 return;
             
+            case 0xFF0F:                                // Interrupt Flag (IF)
+                _interruptFlags = (byte)(value & 0x1F); // Only low 5 interrupt bits are used.
+                return;
+            
             case >= 0xFF80 and <= 0xFFFE:               // Write into the high RAM.
                 _highRam[address - 0xFF80] = value;
+                return;
+            
+            case 0xFFFF:                                 // Interrupt Enable (IE)
+                _interruptEnable = (byte)(value & 0x1F); // Only low 5 interrupt bits are used.
                 return;
             
             default: throw new NotImplementedException($"Write not implemented for 0x{address:X4}");
